21634b4d49f825b54a7423b51695a6d802ca94c0
Leandro Gómez <leandrogomezmdev@gmail.com>
2025-09-08 00:24:57 -0300

    Initialize Next.js project with config and assets
    Set up a new Next.js project including configuration files, ESLint and PostCSS setup, TypeScript config, public SVG assets, global styles, and initial app layout and page. Updated README with setup and usage instructions.

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+
+import "./globals.css";
+
+export const metadata = {
+  title: "App Auth JWT",
+  description: "Autenticación JWT con Next.js",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
